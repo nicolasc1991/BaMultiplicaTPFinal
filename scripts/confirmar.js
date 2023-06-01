8335b62597983d87fc345bfbfe7d33b862ba32a5
Nicolas <ncfynnc@gmail.com>
2023-06-01 14:47:05 -0300

    redireccionando al inicio si cancela la adopcion

--- scripts/confirmar.js
+++ scripts/confirmar.js
@@ -3,5 +3,6 @@ function confirmarEnvio() {
     alert("Gracias por adoptar")
   } else {
     alert("Te esperamos cuando estes seguro de adoptar")
+    window.location.href = "https://nicolasc1991.github.io/BaMultiplicaTPFinal/";
   }
 }
